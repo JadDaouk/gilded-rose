d155be1fa704dbdd26c66898025e63871d16a048
Jad <jad.daouk@epsi.fr>
2022-12-16 17:26:58 +0100

    Update Main.java

--- src/main/java/gildedrose/Main.java
+++ src/main/java/gildedrose/Main.java
@@ -1,32 +1,8 @@
 package gildedrose;
 
-import gildedrose.auctionHouse.AuctionHouseInteractor;
-import gildedrose.auctionHouse.input.AuctionHouseConsoleController;
-import gildedrose.balance.repositories.InMemoryBalanceRepository;
-import gildedrose.item.repositories.InMemoryItemsRepository;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        // shopConsoleController = new ShopConsoleController();
-
-        //shopConsoleController.shopInteractor.updateInventory();
-
-        //SellItemRequest sellItemRequest = new SellItemRequest("Generic", 7, 10);
-        //shopConsoleController.shopInteractor.sellItem(sellItemRequest);
-        //shopConsoleController.shopInteractor.sellItem(sellItemRequest);
-//
-        //AuctionHouseInteractor auctionHouseInteractor;
-        //InMemoryItemsRepository inMemoryItemsRepository;
-        //InMemoryBalanceRepository inMemoryBalanceRepository;
-        ///inMemoryBalanceRepository = new InMemoryBalanceRepository();
-        ///inMemoryItemsRepository = new InMemoryItemsRepository();
-        //auctionHouseInteractor = new AuctionHouseInteractor(inMemoryItemsRepository,inMemoryBalanceRepository);
-        AuctionHouseConsoleController actionHouseConsoleController = new AuctionHouseConsoleController();
-        actionHouseConsoleController.console();
-
-
-
+        System.out.println("Not implemented yet");
     }
 }
